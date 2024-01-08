b328b7401c6a14197f5741ef9d0ecdd140abc628
Gnanasekhar <gnanasekharofficial@gmail.com>
2024-01-09 00:00:09 +0530

    react app created and tailwind css connected

---
+++ Garuda/Client/src/containers/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  return <div className="w-screen h-screen flex items-center justify-center text-yellow-600 font-semibold"> App </div>;
+  
+};
+
+export default App;
