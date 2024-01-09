6586ca006f1e6fa96a315df4f0020d87d0a8b3c1
Gnanasekhar <gnanasekharofficial@gmail.com>
2024-01-09 18:47:29 +0530

    added routes

--- Garuda/Client/src/containers/App.jsx
+++ Garuda/Client/src/containers/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,{Suspense} from 'react';
+import { Routes,Route, } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+import { Home, } from '../pages';
 
 const App = () => {
-  return <div className="w-screen h-screen flex items-center justify-center text-yellow-600 font-semibold"> App </div>;
-  
+  return (
+    <Suspense fallback = {<div>Loading....</div>}>
+        <Routes>
+            <Route element = {<Layout/>}>
+                <Route path = '/' element = {<Home/>}/>
+
+
+             
+                
+            </Route>
+        </Routes>
+    </Suspense>
+
+  );
 };
 
 export default App;
